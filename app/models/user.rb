465a69e1e521f6e72569547a81f93e1a4a1a8a61
florian bernier <florianbernier.soa@gmail.com>
2024-03-05 16:59:19 +0100

    generate controller profiles & edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :challenges
-  validates :username, :age, presence: true
 end
