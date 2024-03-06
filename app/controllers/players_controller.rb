0ca27c5ff788455973f5e7cda1b787c527adeeb1
florian bernier <florianbernier.soa@gmail.com>
2024-03-06 21:45:47 +0100

    index id player design bootstrap

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,12 @@
-class BarPlayersController < ApplicationController
+class PlayersController < ApplicationController
+  before_action :set_bar, only: %i[index]
   def index
+    @users = User.all.where(nearest_bar_id: @bar)
+  end
 
+  private
+
+  def set_bar
+    @bar = Bar.find(params[:bar_id])
   end
 end
