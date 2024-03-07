0a2f21dc4c12ab8fa6756a1929a8c0c0d79b7979
Romain Bernier <romain.bernier@live.fr>
2024-03-07 14:55:00 +0100

    map init

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,6 +1,12 @@
 class BarsController < ApplicationController
   def index
     @bars = Bar.all
+    @markers = @bars.geocoded.map do |bar|
+      {
+        lat: bar.latitude,
+        lng: bar.longitude
+      }
+    end
   end
 
   def show
