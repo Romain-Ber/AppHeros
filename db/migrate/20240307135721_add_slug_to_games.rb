e1ef8ee58bebcd86fa6a2afd67f4580dedac25e3
Romain Bernier <romain.bernier@live.fr>
2024-03-07 17:18:53 +0100

    revert db migration

--- db/migrate/20240307135721_add_slug_to_games.rb
+++
@@ -1,5 +0,0 @@
-class AddSlugToGames < ActiveRecord::Migration[7.1]
-  def change
-    add_column :games, :slug, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -76,6 +76,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_07_121905) do
 
   create_table "games", force: :cascade do |t|
     t.string "name"
+    t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
