f84fd872bb85854f22dc083404a082c746196e97
florian bernier <florianbernier.soa@gmail.com>
2024-03-07 17:38:47 +0100

    structure seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_07_121905) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_07_135721) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,9 +76,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_07_121905) do
 
   create_table "games", force: :cascade do |t|
     t.string "name"
-    t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
   end
 
   create_table "scores", force: :cascade do |t|
