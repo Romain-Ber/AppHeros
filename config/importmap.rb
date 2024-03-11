4ea74dec3de97a024d629c8626e09871cd997365
Romain Bernier <romain.bernier@live.fr>
2024-03-11 16:08:04 +0100

    glsl js + layout

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,9 @@ pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "mapbox-gl" # @3.1.2
 pin "process" # @2.0.1
+pin "glslCanvas" # @0.2.6
+pin "global/window", to: "global--window.js" # @4.4.0
+pin "is-function" # @1.0.2
+pin "parse-headers" # @2.0.5
+pin "xhr" # @2.6.0
+pin "xtend" # @4.0.2
