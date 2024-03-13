9eabdc22d1f5b4ea040779d0bce47261870e1a45
cannelle hardy <cannellou56430@gmail.com>
2024-03-13 14:53:04 +0100

    filters and search bar finished back+front

--- app/javascript/controllers/filters_controller.js
+++ app/javascript/controllers/filters_controller.js
@@ -2,20 +2,65 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="filters"
 export default class extends Controller {
-  static targets=["bar"]
-  connect() {
+  static targets=["bar", "button"]
 
-  }
-
-  resetFilters() {
+  resetFilters(event) {
     this.barTargets.forEach(bar => {
       bar.classList.remove("d-none")
     })
+    this.activateButton(event)
   }
 
-  applicateFilter() {
+  maxPlayer(event) {
+    // console.log(event.currentTarget)
+    const count = parseInt(event.currentTarget.getAttribute("data-count"))
+    // console.log(count)
     this.barTargets.forEach(bar => {
-      bar.classList.
+      const barCount = parseInt(bar.getAttribute("data-count"))
+      if (barCount < count) {
+        bar.classList.add("d-none")
+      } else {
+        bar.classList.remove("d-none")
+      }
+    })
+    this.activateButton(event)
+  }
+
+  minPlayer(event) {
+    const count = parseInt(event.currentTarget.getAttribute("data-count"))
+    this.barTargets.forEach(bar => {
+      const barCount = parseInt(bar.getAttribute("data-count"))
+      if (barCount > count) {
+        bar.classList.add("d-none")
+      } else {
+        bar.classList.remove("d-none")
+      }
+    })
+    this.activateButton(event)
+  }
+
+  proximity(event) {
+    const bars = JSON.parse(event.currentTarget.getAttribute("data-proximity"))
+
+    this.barTargets.forEach(bar => {
+      // console.log(bar.getAttribute("data-id"))
+      if (bars.includes(parseInt(bar.getAttribute("data-id")))) {
+        bar.classList.remove("d-none")
+      } else {
+        bar.classList.add("d-none")
+      }
+    })
+    this.activateButton(event)
+  }
+
+  activateButton(event) {
+    console.log(window.location.href)
+    this.buttonTargets.forEach(button => {
+      if (button === event.currentTarget) {
+        button.classList.add("active")
+      } else {
+        button.classList.remove("active")
+      }
     })
   }
 }
