bdc9948c67d1f56108ff9de3f46b7635802cf96c
Romain Bernier <romain.bernier@live.fr>
2024-03-14 15:59:42 +0100

    oefkefjk

--- db/knnll.rb
+++ db/knnll.rb
@@ -4,11 +4,21 @@ knnll.latitude = lapiste.latitude
 knnll.longitude = lapiste.longitude
 knnll.save
 
-lapiste.description
+romain = User.find_by(email: "romain@gmail.com")
+romain.latitude = lapiste.latitude
+romain.longitude = lapiste.longitude
+romain.save
 
-randomBar = Bar.first
+lapiste.description = "La Piste  à Rennes est un lieu animé et convivial, idéal pour une soirée détendue entre amis. L'ambiance décontractée, la musique entraînante et la sélection de boissons variée en font un choix populaire."
+lapiste.save
+
 lapisterUsers = User.find_by(nearest_bar_id: lapiste.id)
 lapisterUsers.each do |user|
-  user.nearest_bar_id = randomBar.id
-  user.save
+  70.times do
+    Score.create!(
+      score: rand(1..5),
+      bar: lapiste,
+      user: user
+    )
+  end
 end
