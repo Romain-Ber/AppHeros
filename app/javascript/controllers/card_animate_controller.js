91308c7f6bccbeb622ab1d75961c838f7c2451db
Rakido <35547354+Rakido@users.noreply.github.com>
2024-03-14 11:11:14 +0100

    add intersection observer to card index

---
+++ app/javascript/controllers/card_animate_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+  static targets = ["card"]
+
+  connect() {
+    //console.log(this.cardTargets)
+    // Nouvel instance d'observer qui bouffe les projets
+    const projectObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // Qu'est-ce qui se passe quand l'élément est visible
+          entry.target.style.opacity = 1;
+          entry.target.style.transform = "scale(1)";
+        } else {
+          // Qu'est-ce qui se passe quand il est pas visible
+          entry.target.style.opacity = 0;
+          entry.target.style.transform = "scale(1.3)";
+        }
+      });
+    },
+    {
+      threshold: 0.5
+    }
+    );
+    this.cardTargets.forEach((card, i) => {
+      projectObserver.observe(card);
+    });
+  }
+}
+
